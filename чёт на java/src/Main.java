59f1e89a2a57ce9937e25c9cce2b44a0a13bdff4
iglobrix12 <152422453+iglobrix12@users.noreply.github.com>
2024-01-29 18:18:31 +0300

    Add files via upload

---
+++ чёт на java/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        say test = new say();
+        test.say("???", "довно хотел сказать тебе");
+        test.say("[???]", "я рыба...............");
+    }
+}
---
+++ чёт на java/src/say.java
@@ -0,0 +1,5 @@
+public class say {
+    public void say(String namehero, String text) {
+        System.out.println("[" + namehero + "] " + text + ".");
+    }
+}
